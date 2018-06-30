b1e2d93f2e3eb53680eb52810b8fdd8ded866d1d
Ben Spark <bhrugen1990@gmail.com>
2018-06-29 20:57:43 -0500

    Initial Commit

--- GraniteHouse/Startup.cs
+++ GraniteHouse/Startup.cs
@@ -19,7 +19,7 @@ namespace GraniteHouse
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
