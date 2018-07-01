7756078273b2e6b09ec68bc21dcc42302e1cd379
Ben Spark <bhrugen1990@gmail.com>
2018-06-30 23:46:05 -0500

    Section 4 - Add ProductTypes Controller

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+    }
+}
