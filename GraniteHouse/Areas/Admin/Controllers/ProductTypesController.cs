a7e017f0720169d19295967dd553d6bc919c48bb
Ben Spark <bhrugen1990@gmail.com>
2018-07-01 05:17:14 -0500

    Section 4 - Product Types Create Action Method

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Areas.Admin.Controllers
 {
@@ -18,10 +20,30 @@ namespace GraniteHouse.Areas.Admin.Controllers
             _db = db;
         }
 
-
         public IActionResult Index()
         {
             return View(_db.ProductTypes.ToList());
         }
+
+        //GET Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
     }
 }
