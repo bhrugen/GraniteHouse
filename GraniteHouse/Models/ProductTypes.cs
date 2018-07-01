86a2da24efe6c1e88837cd91a251ef9e58eb8bb1
Ben Spark <bhrugen1990@gmail.com>
2018-06-30 22:49:43 -0500

    Section 3 - Add Model for Product Type

---
+++ GraniteHouse/Models/ProductTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
