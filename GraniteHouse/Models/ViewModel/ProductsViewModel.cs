4e79fe060210b92875ac0768b6989936af101632
Ben Spark <bhrugen1990@gmail.com>
2018-07-06 09:35:10 -0500

    Section 5 - Products View Model

---
+++ GraniteHouse/Models/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModel
+{
+    public class ProductsViewModel
+    {
+        public Products Products { get; set; }
+        public IEnumerable<ProductTypes> ProductTypes { get; set; }
+        public IEnumerable<SpecialTags> SpecialTags { get; set; }
+
+    }
+}
