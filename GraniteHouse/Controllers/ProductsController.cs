a335acc385e7848c9fea62cb92123e57be91fec8
Ben Spark <bhrugen1990@gmail.com>
2018-07-06 09:43:28 -0500

    Section 5 - Products : Index Action Method

---
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
+            return View(await products.ToListAsync());
+        }
+    }
+}
