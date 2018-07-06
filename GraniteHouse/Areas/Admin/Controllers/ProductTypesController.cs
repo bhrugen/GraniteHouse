4618b9fcae5761ee76ebaa527e4bd1640a501e8a
Ben Spark <bhrugen1990@gmail.com>
2018-07-06 07:55:10 -0500

    Section 4 - Product Type : Details Action method and View

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -82,5 +82,22 @@ namespace GraniteHouse.Areas.Admin.Controllers
             return View(productTypes);
         }
 
+        //GET Details Action Method
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
     }
 }
