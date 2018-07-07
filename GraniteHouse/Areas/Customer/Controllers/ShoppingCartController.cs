00807a7b2c7383744c576d68e0435c89d6aa2d78
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 01:08:15 -0500

    Section 7 - Shopping Cart : Get Action Method

---
+++ GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Extensions;
+using GraniteHouse.Models;
+using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+
+        public ShoppingCartController( ApplicationDbContext db)
+        {
+            _db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        //Get Index Shopping Cart
+        public async  Task<IActionResult> Index()
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if(lstShoppingCart.Count>0)
+            {
+                foreach(int cartItem in lstShoppingCart)
+                {
+                    Products prod = _db.Products.Include(p => p.SpecialTags).Include(p => p.ProductTypes).Where(p => p.Id == cartItem).FirstOrDefault();
+                    ShoppingCartVM.Products.Add(prod);
+                }
+            }
+            return View(ShoppingCartVM);
+        }
+    }
+}
