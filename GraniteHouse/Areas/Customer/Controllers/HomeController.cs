056a322320587b7beeeaf4d4c0d52ba5f1b49ac7
Ben Spark <bhrugen1990@gmail.com>
2018-07-06 22:38:49 -0500

    Section 6 - Home Page : Index Action Method

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -5,15 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GraniteHouse.Models;
+using GraniteHouse.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).ToListAsync();
+
+            return View(productList);
         }
 
         public IActionResult About()
