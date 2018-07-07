8eef251b6fbdbb8b37d2f9c467d9ecc9f079e85f
Ben Spark <bhrugen1990@gmail.com>
2018-07-06 22:26:16 -0500

    Section 5 - Product : Delete Post Action Method

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModel;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Hosting.Internal;
@@ -198,6 +199,34 @@ namespace GraniteHouse.Controllers
             }
 
             return View(ProductsVM);
+        }
+
+        //POST : Delete
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            Products products = await _db.Products.FindAsync(id);
+
+            if(products==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(products.Image);
+
+                if(System.IO.File.Exists(Path.Combine(uploads,products.Id+extension)))
+                {
+                    System.IO.File.Delete(Path.Combine(uploads, products.Id + extension));
+                }
+                _db.Products.Remove(products);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
         }
 
     }
