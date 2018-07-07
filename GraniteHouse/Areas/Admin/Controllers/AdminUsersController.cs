ecf85421badf74d3f322ff212cb1dc86524f364c
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 12:11:16 -0500

    Section 8 -  Admin Users Edit Action Methods

--- GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -21,6 +22,47 @@ namespace GraniteHouse.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ApplicationUser.ToList());
+        }
+
+        //Get Edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(id==null || id.Trim().Length==0)
+            {
+                return NotFound();
+            }
+
+            var userFromDb = await _db.ApplicationUser.FindAsync(id);
+            if(userFromDb==null)
+            {
+                return NotFound();
+            }
+
+            return View(userFromDb);
+        }
+
+
+        //Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit (string id, ApplicationUser applicationUser)
+        {
+            if(id!=applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                ApplicationUser userFromDb = _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefault();
+                userFromDb.Name = applicationUser.Name;
+                userFromDb.PhoneNumber = applicationUser.PhoneNumber;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
         }
     }
 }
