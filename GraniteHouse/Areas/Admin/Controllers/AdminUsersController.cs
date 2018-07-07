43edc062db2217cb8d18021309e5f93065ee4550
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 10:28:14 -0500

    Section 8 - Add Admin User Controller

---
+++ GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminUsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public AdminUsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUser.ToList());
+        }
+    }
+}
