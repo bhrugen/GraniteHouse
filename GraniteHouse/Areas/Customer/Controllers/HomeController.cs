8e11055e33dcd6204f385e9df15ef0957b8de72b
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 00:11:01 -0500

    Section 6 - Details : Remove Action Method

--- GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -53,6 +53,22 @@ namespace GraniteHouse.Controllers
 
         }
 
+        public IActionResult Remove(int id)
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if(lstShoppingCart.Count>0)
+            {
+                if(lstShoppingCart.Contains(id))
+                {
+                    lstShoppingCart.Remove(id);
+                }
+            }
+
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction(nameof(Index));
+        }
+
 
 
 
