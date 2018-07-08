969417efa5af478d28389941eeea1043b84911d0
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 19:01:23 -0500

    Section 9 - Appointment View Model

---
+++ GraniteHouse/Models/ViewModel/AppointmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModel
+{
+    public class AppointmentViewModel
+    {
+        public List<Appointments> Appointments { get; set; }
+    }
+}
