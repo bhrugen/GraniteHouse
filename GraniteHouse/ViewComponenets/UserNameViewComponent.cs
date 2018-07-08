22c0d9079e2d8c70d031f1f80e9294bead9ebfe6
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 23:43:52 -0500

    Section 10 - View Component Method

---
+++ GraniteHouse/ViewComponenets/UserNameViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.ViewComponenets
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUser.Where(u => u.Id == claims.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
