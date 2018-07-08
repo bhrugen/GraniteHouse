6a59d35ee4c50eb13f2e37d86e039db8e02b28cd
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 19:49:05 -0500

    Section 9 - Appointment Index Get Action Method

--- GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModel;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Areas.Admin.Controllers
 {
@@ -12,9 +16,33 @@ namespace GraniteHouse.Areas.Admin.Controllers
     [Area("Admin")]
     public class AppointmentsController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentsController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentVM = new AppointmentViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            appointmentVM.Appointments = _db.Appointments.Include(a => a.SalesPerson).ToList();
+            if(User.IsInRole(SD.AdminEndUser))
+            {
+                appointmentVM.Appointments = appointmentVM.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+            }
+
+            return View(appointmentVM);
         }
     }
 }
