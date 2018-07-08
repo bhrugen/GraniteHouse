d909ead6992bad5b114ef792ce15b824476ee825
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 22:39:49 -0500

    Section 10 - Add Paging Info Model

---
+++ GraniteHouse/Models/PagingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+
+        public int ItemsPerPage{ get; set; }
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+
+        //This will be used to build URL
+        public string urlParam { get; set; }
+    }
+}
