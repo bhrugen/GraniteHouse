1363e606951cddf54588e14131c26c07cf1c3559
Ben Spark <bhrugen1990@gmail.com>
2018-07-07 21:39:55 -0500

    Section 9 - Appointment : Edit View Part 2

--- GraniteHouse/Extensions/IEnumerableExtensions.cs
+++ GraniteHouse/Extensions/IEnumerableExtensions.cs
@@ -18,5 +18,20 @@ namespace GraniteHouse.Extensions
                        Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
                    };
         }
+
+        public static IEnumerable<SelectListItem> ToSelectListItemString<T>(this IEnumerable<T> items, string selectedValue)
+        {
+            if(selectedValue==null)
+            {
+                selectedValue = "";
+            }
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
+                   };
+        }
     }
 }
